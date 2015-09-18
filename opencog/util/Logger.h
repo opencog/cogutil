e19f00d52aeabe31959b3d6534c1d675dfde3396
Nil Geisweiller <ngeiswei@gmail.com>
2015-09-18 15:59:22 +0300

    Add macros for laziness in Logger
    See LoggerUTest::testLazyLogger() for usage example

--- opencog/util/Logger.h
+++ opencog/util/Logger.h
@@ -300,6 +300,13 @@ private:
 // singleton instance (following Meyer's design pattern)
 Logger& logger();
 
+// Macro to not evaluate the stream if log level is disabled
+#define LAZY_LOGGER_ERROR if(logger().isErrorEnabled()) logger().error()
+#define LAZY_LOGGER_WARN if(logger().isWarnEnabled()) logger().warn()
+#define LAZY_LOGGER_INFO if(logger().isInfoEnabled()) logger().info()
+#define LAZY_LOGGER_DEBUG if(logger().isDebugEnabled()) logger().debug()
+#define LAZY_LOGGER_FINE if(logger().isFineEnabled()) logger().fine()
+
 /** @}*/
 }  // namespace opencog
 
