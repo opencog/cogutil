19df401816904e17708b8181bdfb1f31611865e3
Linas Vepstas <linasvepstas@gmail.com>
2017-01-14 19:20:06 -0600

    Attempt a fancier version

--- opencog/util/sigslot.h
+++ opencog/util/sigslot.h
@@ -11,7 +11,7 @@
 // frames to do its thing. Eleven! Really!) So this class avoids that.
 //
 // We don't need anything fancy; this is super-minimalist in design.
-// It is thread-safe.  It works!
+// It is thread-safe. It works!
 //
 // Example usage:
 //
@@ -19,10 +19,27 @@
 //    printf ("ola %d (%d) [%d %d %d]\n", x, y.size(), y[0], y[1], y[2]);
 // }
 //
-// SigSlot<int, std::vector<int>> siggy;
-// siggy.connect(foo);
-// siggy.emit(42, {68,69,70});
+// main () {
+//     SigSlot<int, std::vector<int>> siggy;
+//     siggy.connect(foo);
+//     siggy.emit(42, {68,69,70});
+// }
 //
+// The following also works:
+//
+// class Bar { public:
+//     void baz(int x, std::vector<int> y) {
+//         printf ("ciao %d\n", x);
+//     }};
+//
+// main () {
+//     Bar bell;
+//     SigSlot<int, std::vector<int>> siggy;
+//     auto glub = std::bind(&Bar::baz, bell,
+//              std::placeholders::_1, std::placeholders::_2);
+//     siggy.connect(glub);
+//     siggy.emit(42, {68,69,70});
+// }
 
 template <typename... ARGS>
 class SigSlot
@@ -32,6 +49,7 @@ class SigSlot
 		// HACK ALERT -- use std::map, not std::set here, for only
 		// one reason: so that we get a natural operator-less, which
 		// is needed for the insert() to work. 
+
 		mutable std::map<int, std::function<void(ARGS...)>> _slots;
 		mutable int _slot_id;
 
@@ -46,6 +64,32 @@ class SigSlot
 			_slots.insert(std::make_pair(_slot_id, fn));
 			return _slot_id;
 		}
+
+#if BORKEN_FOR_SOME_REASON
+		// Connect member of a given object.
+		// XXX Something like this should work, but I can't get it to go.
+		//
+		// class Bar { public:
+		//     void baz(int x, std::vector<int> y) {
+		//         printf ("ciao %d\n", x);
+		//     }
+		// };
+		//
+		// main() {
+		//     Bar bell;
+		//     SigSlot<int, std::vector<int>> siggy;
+		//     siggy.connect_m(&Bar::baz, bell);
+		//     siggy.emit(42, {68,69,70});
+		// }
+		template <typename FN, typename... AR>
+		int connect(FN&& fn, AR&& ... ag)
+		{
+			std::lock_guard<std::mutex> lck(_mtx);
+			_slot_id++;
+			_slots.insert({_slot_id, std::bind(fn, ag ...)});
+			return _slot_id;
+		}
+#endif
 
 		void disconnect(int id)
 		{
