676d50b42b85ec22b99a7611acb8f1055008f673
Linas Vepstas <linasvepstas@gmail.com>
2017-05-22 13:57:40 -0500

    Add a concurrent set.

---
+++ opencog/util/concurrent_set.h
@@ -0,0 +1,221 @@
+/*
+ * opencog/util/concurrent_set.h
+ *
+ * Based off of http://www.justsoftwaresolutions.co.uk/threading/implementing-a-thread-safe-queue-using-condition-variables.html
+ * Original version by Anthony Williams
+ * Modifications by Michael Anderson
+ * Modified by Linas Vepstas
+ * Updated API to more closely resemble the proposed
+ * ISO/IEC JTC1 SC22 WG21 N3533 C++ Concurrent Queues
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OC_CONCURRENT_SET_H
+#define _OC_CONCURRENT_SET_H
+
+#include <condition_variable>
+#include <set>
+#include <exception>
+#include <mutex>
+
+/** \addtogroup grp_cogutil
+ *  @{
+ */
+
+//! Represents a thread-safe first in-first out list.
+///
+/// Implements a thread-safe queue: any thread can push stuff onto the
+/// queue, and any other thread can remove stuff from it.  If the queue
+/// is empty, the thread attempting to remove stuff will block.  If the
+/// queue is empty, and something is added to the queue, and there is
+/// some thread blocked on the queue, then that thread will be woken up.
+///
+/// The function provided here is almost identical to that provided by
+/// the pool class (also in this directory), but with a fancier API that
+/// allows cancellation, and other minor utilities. This API is also
+/// most easily understood as a producer-consumer API, with producer
+/// threads adding stuff to the queue, and consumer threads removing
+/// them.  By contrast, the pool API is a borrow-and-return API, which
+/// is really more-or-less the same thing, but just uses a different
+/// mindset.  This API also matches the proposed C++ standard for this
+/// basic idea.
+
+template<typename Element>
+class concurrent_set
+{
+private:
+    std::set<Element> the_set;
+    mutable std::mutex the_mutex;
+    std::condition_variable the_cond;
+    bool is_canceled;
+
+public:
+    concurrent_set()
+        : the_set(), the_mutex(), the_cond(), is_canceled(false)
+    {}
+    concurrent_set(const concurrent_set&) = delete;  // disable copying
+    concurrent_set& operator=(const concurrent_set&) = delete; // no assign
+
+    struct Canceled : public std::exception
+    {
+        const char * what() { return "Cancellation of wait on concurrent_set"; }
+    };
+
+    /// Insert the Element into the set; copies the item.
+    void insert(const Element& item)
+    {
+        std::unique_lock<std::mutex> lock(the_mutex);
+        if (is_canceled) throw Canceled();
+        the_set.insert(item);
+        lock.unlock();
+        the_cond.notify_one();
+    }
+
+    /// Insert the Element into the set, by moving it.
+    void insert(Element&& item)
+    {
+        std::unique_lock<std::mutex> lock(the_mutex);
+        if (is_canceled) throw Canceled();
+        the_set.insert(std::move(item));
+        lock.unlock();
+        the_cond.notify_one();
+    }
+
+    /// Return true if the set is empty.
+    bool is_empty() const
+    {
+        std::lock_guard<std::mutex> lock(the_mutex);
+        if (is_canceled) throw Canceled();
+        return the_set.empty();
+    }
+
+    /// Return the size of the set.
+    /// Since the set is time-varying, the size may become incorrect
+    /// shortly after this method returns.
+    unsigned int size() const
+    {
+        std::lock_guard<std::mutex> lock(the_mutex);
+        return the_set.size();
+    }
+
+    /// Try to get an element in the set. Return true if success,
+    /// else return false. The element is removed from the set.
+    bool try_get(Element& value)
+    {
+        std::lock_guard<std::mutex> lock(the_mutex);
+        if (is_canceled) throw Canceled();
+        if (the_set.empty())
+        {
+            return false;
+        }
+
+        auto it = the_set.begin();
+        value = *it;
+        the_set.erase(it);
+        return true;
+    }
+
+    /// Get an item from the set. Block if the set is empty.
+    /// The element is remove from the set, before this returns.
+    void get(Element& value)
+    {
+        std::unique_lock<std::mutex> lock(the_mutex);
+
+        // Use two nested loops here.  It can happen that the cond
+        // wakes up, and yet the set is empty.
+        do
+        {
+            while (the_set.empty() and not is_canceled)
+            {
+                the_cond.wait(lock);
+            }
+            if (is_canceled) throw Canceled();
+        }
+        while (the_set.empty());
+
+        auto it = the_set.begin();
+        value = *it;
+        the_set.erase(it);
+    }
+
+    Element get()
+    {
+        Element value;
+        get(value);
+        return value;
+    }
+
+    std::set<Element> wait_and_take_all()
+    {
+        std::unique_lock<std::mutex> lock(the_mutex);
+
+        // Use two nested loops here.  It can happen that the cond
+        // wakes up, and yet the queue is empty.
+        do
+        {
+            while (the_set.empty() and not is_canceled)
+            {
+                the_cond.wait(lock);
+            }
+            if (is_canceled) throw Canceled();
+        }
+        while (the_set.empty());
+
+        std::set<Element> retval;
+        std::swap(retval, the_set);
+        return retval;
+    }
+
+    /// A weak barrier.  This will block as long as the queue is empty,
+    /// returning only when the queue isn't. It's "weak", because while
+    /// it waits, other threads may push and then pop something from
+    /// the queue, while this thread slept the entire time. However,
+    /// if this call does return, then the queue is almost surely not
+    /// empty.  "Almost surely" means that none of the other threads
+    /// that are currently waiting to pop from the queue will be woken.
+    void barrier()
+    {
+        std::unique_lock<std::mutex> lock(the_mutex);
+
+        while (the_set.empty() and not is_canceled)
+        {
+            the_cond.wait(lock);
+        }
+        if (is_canceled) throw Canceled();
+    }
+
+    void cancel_reset()
+    {
+       // This doesn't lose data, but it instead allows new calls
+       // to not throw Canceled exceptions
+       std::lock_guard<std::mutex> lock(the_mutex);
+       is_canceled = false;
+    }
+
+    void cancel()
+    {
+       std::unique_lock<std::mutex> lock(the_mutex);
+       if (is_canceled) throw Canceled();
+       is_canceled = true;
+       lock.unlock();
+       the_cond.notify_all();
+    }
+
+};
+/** @}*/
+
+#endif // __CONCURRENT_SET__
