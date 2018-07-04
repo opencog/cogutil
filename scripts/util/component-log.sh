11483ff6e2b04c76ad74b504e295086f5fe9049f
Nil Geisweiller <ngeiswei@gmail.com>
2018-07-04 09:25:18 +0300

    Add program help in component-log.sh

--- scripts/util/component-log.sh
+++ scripts/util/component-log.sh
@@ -3,26 +3,21 @@
 # Given a component and a log file, filter the log so that only the
 # messages from the component are output.
 
-if [[ $# == 0 || $# > 2 ]]; then
-    echo "Error: Wrong number of arguments"
-    echo "Description: Given a component and log file,"
-    echo "             filter the log so that only messages"
-    echo "             from the component are output."
-    echo "Usage: $0 COMPONENT [LOG_FILE]"
-    echo "If no log file is provided as second argument, then use stdin"
-    exit 1
-fi
+set -u
 
-COMPONENT="$1"
-LOGFILE="$2"
+#############
+# Constants #
+#############
 
 readonly date_re='[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}'
 readonly time_re='[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{3}'
 readonly timestamp_re="\[${date_re} ${time_re}\]"
 readonly loglevel_re="\[(ERROR|WARN|INFO|DEBUG|FINE)\]"
-readonly component_re="\[$COMPONENT\]"
 readonly logline_re="$timestamp_re $loglevel_re .*"
-readonly component_logline_re="$timestamp_re $loglevel_re $component_re .*"
+
+#############
+# Functions #
+#############
 
 keep_printing=n                 # for printing multiline messages
 grep_component_lines()
@@ -37,6 +32,35 @@ grep_component_lines()
         echo "$line"
     fi
 }
+
+usage()
+{
+    echo "Description: Given a component and log file,"
+    echo "             filter the log so that only messages"
+    echo "             from the component are output."
+    echo "Usage: $0 COMPONENT [LOG_FILE]"
+    echo "If no log file is provided as second argument, then use stdin"
+}
+
+########
+# Main #
+########
+
+if [[ $# == 0 || $# > 2 ]]; then
+    echo "Error: Wrong number of arguments"
+    usage
+    exit 1
+fi
+
+if [[ $1 == "-h" || $1 == "--help" || $1 == "-?" ]]; then
+    usage
+    exit 0
+fi
+
+readonly COMPONENT="$1"
+readonly LOGFILE="$2"
+readonly component_re="\[$COMPONENT\]"
+readonly component_logline_re="$timestamp_re $loglevel_re $component_re .*"
 
 if [[ "$LOGFILE" > 0 ]]; then
     while read; do
