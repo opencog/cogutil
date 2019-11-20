d0179e9e1444a7c82b75045b49157987ef6f1e31
Linas Vepstas <linasvepstas@gmail.com>
2019-11-20 13:59:38 -0600

    Restore a compatibility function

--- opencog/util/concurrent_stack.h
+++ opencog/util/concurrent_stack.h
@@ -174,6 +174,11 @@ public:
         return value;
     }
 
+    /// XXX DO NOT USE! This has a C++ signature incompatible with
+    /// the other pop, immediately above, thus wrecking automatic
+    /// C++ overloading!
+    Element pop() { return value_pop(); }
+
     std::stack<Element> wait_and_take_all()
     {
         std::unique_lock<std::mutex> lock(the_mutex);
