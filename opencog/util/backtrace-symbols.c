3cae36895c0e23c4013c043e02fac57676b3789d
Linas Vepstas <linasvepstas@gmail.com>
2020-04-15 10:52:18 -0500

    Ifdef the old vs new bfd API

--- opencog/util/backtrace-symbols.c
+++ opencog/util/backtrace-symbols.c
@@ -115,6 +115,7 @@ static void find_address_in_section(bfd *abfd, asection *section, void *data)
 
     if (spot->found) return;
 
+#ifdef NEW_BFD_API
     if ((bfd_section_flags(section) & SEC_ALLOC) == 0) return;
 
     vma = bfd_section_vma(section);
@@ -122,7 +123,15 @@ static void find_address_in_section(bfd *abfd, asection *section, void *data)
 
     size = bfd_section_size(section);
     if (spot->pc >= vma + size) return;
+#else
+    if ((bfd_get_section_flags(abfd, section) & SEC_ALLOC) == 0) return;
 
+    vma = bfd_get_section_vma(abfd, section);
+    if (spot->pc < vma) return;
+
+    size = bfd_section_size(abfd, section);
+    if (spot->pc >= vma + size) return;
+#endif
     spot->found = bfd_find_nearest_line(abfd, section, spot->syms, spot->pc - vma,
                                         &(spot->filename), &(spot->functionname), &(spot->line));
 }
