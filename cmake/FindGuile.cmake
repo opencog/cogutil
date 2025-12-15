b04fed8fa229382513c145becf97871d3f8744bf
Linas Vepstas <linasvepstas@gmail.com>
2025-12-15 17:46:14 +0000

    FindGuile with pkgconfig
    This avoids the hard-coded paths that are tripping up guix.

--- cmake/FindGuile.cmake
+++ cmake/FindGuile.cmake
@@ -1,50 +1,29 @@
-# Copyright (c) 2008, 2014 OpenCog.org (http://opencog.org)
+# Copyright (c) 2008, 2014, 2025 OpenCog.org (http://opencog.org)
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-# - Try to find Guile; Once done this will define
+# - Try to find Guile using pkg-config; Once done this will define
 #
 # GUILE_FOUND - system has the GUILE library
 # GUILE_INCLUDE_DIRS - the GUILE include directory
 # GUILE_LIBRARIES - The libraries needed to use GUILE
 
+FIND_PACKAGE(PkgConfig)
 
-# Look for the header file
-# Look for guile-3.0 first, then 2.2
-# Macports for OSX puts things in /opt/local
-FIND_PATH(GUILE_INCLUDE_DIR libguile.h
-	/usr/include/guile/3.0
-	/usr/local/include/guile/3.0
-	/opt/local/include/guile/3.0
+# Try guile-3.0 first, then 2.2
+PKG_CHECK_MODULES(GUILE guile-3.0 QUIET)
+IF(NOT GUILE_FOUND)
+	PKG_CHECK_MODULES(GUILE guile-2.2 QUIET)
+ENDIF()
 
-	/usr/include/guile/2.2
-	/usr/local/include/guile/2.2
-	/opt/local/include/guile/2.2
+IF(GUILE_FOUND)
+	# pkg-config sets GUILE_INCLUDE_DIRS and GUILE_LIBRARIES
+	# but we also need GUILE_INCLUDE_DIR (singular) for version check
+	LIST(GET GUILE_INCLUDE_DIRS 0 GUILE_INCLUDE_DIR)
+ENDIF()
 
-	/opt/homebrew/include/guile/3.0
-)
-
-# Look for the library
-FIND_LIBRARY(GUILE_LIBRARY NAMES guile-3.0 guile-2.2 PATHS
-	/usr/lib
-	/usr/local/lib
-	/opt/local/lib
-)
-
-# Copy the results to the output variables.
-IF(GUILE_INCLUDE_DIR AND GUILE_LIBRARY)
-	SET(GUILE_FOUND 1)
-	SET(GUILE_LIBRARIES ${GUILE_LIBRARY})
-	SET(GUILE_INCLUDE_DIRS ${GUILE_INCLUDE_DIR})
-ELSE(GUILE_INCLUDE_DIR AND GUILE_LIBRARY)
-	SET(GUILE_FOUND 0)
-	SET(GUILE_LIBRARIES)
-	SET(GUILE_INCLUDE_DIRS)
-ENDIF(GUILE_INCLUDE_DIR AND GUILE_LIBRARY)
-
-
-# check guile's version if we're using cmake >= 2.6
+# Check guile's version from the header file
 IF(GUILE_INCLUDE_DIR)
 	SET(GUILE_VERSION_MAJOR 0)
 	SET(GUILE_VERSION_MINOR 0)
@@ -52,10 +31,9 @@ IF(GUILE_INCLUDE_DIR)
 
 	IF(NOT EXISTS "${GUILE_INCLUDE_DIR}/libguile/version.h")
 		MESSAGE(FATAL_ERROR "Found ${GUILE_INCLUDE_DIR}/libguile.h but not version.h; check your guile installation!")
-	ENDIF(NOT EXISTS "${GUILE_INCLUDE_DIR}/libguile/version.h")
+	ENDIF()
 
 	# Extract the libguile version from the 'version.h' file
-	SET(GUILE_MAJOR_VERSION 0)
 	FILE(READ "${GUILE_INCLUDE_DIR}/libguile/version.h" _GUILE_VERSION_H_CONTENTS)
 
 	STRING(REGEX MATCH "#define SCM_MAJOR_VERSION[	 ]+([0-9])" _MATCH "${_GUILE_VERSION_H_CONTENTS}")
@@ -70,41 +48,38 @@ IF(GUILE_INCLUDE_DIR)
 	# Check found version against required one
 	IF (DEFINED Guile_FIND_VERSION AND ${GUILE_VERSION} VERSION_LESS Guile_FIND_VERSION)
 		SET(GUILE_FOUND FALSE)
-	ELSE ()
-		SET(GUILE_FOUND TRUE)
 	ENDIF ()
-
-ENDIF(GUILE_INCLUDE_DIR)
+ENDIF()
 
 IF (GUILE_FOUND AND GUILE_VERSION_MAJOR EQUAL 3)
 	ADD_DEFINITIONS(-DHAVE_GUILE3)
-ENDIF (GUILE_FOUND AND GUILE_VERSION_MAJOR EQUAL 3)
+ENDIF ()
 
 IF (GUILE_FOUND AND GUILE_VERSION_MAJOR EQUAL 2)
 	ADD_DEFINITIONS(-DHAVE_GUILE2)
 	IF (GUILE_VERSION_MINOR GREATER 0)
 		ADD_DEFINITIONS(-DHAVE_GUILE_2_2)
-	ENDIF (GUILE_VERSION_MINOR GREATER 0)
-ENDIF (GUILE_FOUND AND GUILE_VERSION_MAJOR EQUAL 2)
+	ENDIF ()
+ENDIF ()
 
 # Report the results.
 IF (Guile_FIND_VERSION)
 	SET(_GUILE_VERSION_MESSAGE_STRING "(${GUILE_VERSION} >= ${Guile_FIND_VERSION})")
-ENDIF (Guile_FIND_VERSION)
+ENDIF ()
 
 IF(GUILE_FOUND)
 	IF(NOT GUILE_FIND_QUIETLY)
 		MESSAGE(STATUS "Guile ${_GUILE_VERSION_MESSAGE_STRING} was found.")
-	ENDIF(NOT GUILE_FIND_QUIETLY)
-ELSE(GUILE_FOUND)
+	ENDIF()
+ELSE()
 	SET(GUILE_DIR_MESSAGE "Guile ${_GUILE_VERSION_MESSAGE_STRING} was not found. Make sure GUILE_LIBRARY and GUILE_INCLUDE_DIR are set.")
 	IF(NOT GUILE_FIND_QUIETLY)
 		MESSAGE(STATUS "${GUILE_DIR_MESSAGE}")
-	ELSE(NOT GUILE_FIND_QUIETLY)
+	ELSE()
 		IF(GUILE_FIND_REQUIRED)
 			MESSAGE(FATAL_ERROR "${GUILE_DIR_MESSAGE}")
-		ENDIF(GUILE_FIND_REQUIRED)
-	ENDIF(NOT GUILE_FIND_QUIETLY)
-ENDIF(GUILE_FOUND)
+		ENDIF()
+	ENDIF()
+ENDIF()
 
-MARK_AS_ADVANCED(GUILE_INCLUDE_DIR GUILE_LIBRARY)
+MARK_AS_ADVANCED(GUILE_INCLUDE_DIR GUILE_LIBRARIES)
