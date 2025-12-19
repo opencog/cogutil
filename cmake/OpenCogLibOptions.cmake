1502388088c4ba1e7baddeaaf6ed1e70167a9baf
Linas Vepstas <linasvepstas@gmail.com>
2025-12-19 01:49:14 +0000

    Use GNUInstallDirs to get correct library paths

--- cmake/OpenCogLibOptions.cmake
+++ cmake/OpenCogLibOptions.cmake
@@ -1,27 +1,21 @@
 # -------------------------------------------------
 # Library configuration
 
+# Use CMake's standard GNUInstallDirs for proper lib/lib64 handling
+INCLUDE(GNUInstallDirs)
+
 # This fixes linker issue on OS X
 # See https://cmake.org/cmake/help/v3.0/policy/CMP0042.html
 IF (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 	SET(CMAKE_MACOSX_RPATH 1)
 ENDIF (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 
-# Small hack to handle unixes that use "/usr/lib64" instead of
-# "/usr/lib" as the default lib path on 64 bit archs.
+# Backwards compatibility: derive LIB_DIR_SUFFIX from CMAKE_INSTALL_LIBDIR
+# Many downstream projects still use lib${LIB_DIR_SUFFIX}/opencog
 IF (NOT DEFINED LIB_DIR_SUFFIX)
-	IF (WIN32)
-		# On Windows with MSVC, we don't need this check
-	ELSE (WIN32)
-		EXECUTE_PROCESS(COMMAND ${CMAKE_CXX_COMPILER} -print-search-dirs OUTPUT_VARIABLE PRINT_SEARCH_DIRS_OUTPUT)
-		STRING(REGEX MATCH "\r?\nlibraries:.*\r?\n" COMPILER_LIB_SEARCH_DIRS ${PRINT_SEARCH_DIRS_OUTPUT})
-		IF (NOT ${COMPILER_LIB_SEARCH_DIRS} STREQUAL "")
-			STRING(REGEX MATCH "/lib64/:|/lib64:|/lib64\n" HAS_LIB64 ${COMPILER_LIB_SEARCH_DIRS})
-			IF (NOT ${HAS_LIB64} STREQUAL "")
-				SET(LIB_DIR_SUFFIX "64")
-			ENDIF (NOT ${HAS_LIB64} STREQUAL "")
-		ENDIF (NOT ${COMPILER_LIB_SEARCH_DIRS} STREQUAL "")
-	ENDIF (WIN32)
+	# CMAKE_INSTALL_LIBDIR is typically "lib" or "lib64"
+	# Extract suffix by removing "lib" prefix
+	STRING(REGEX REPLACE "^lib" "" LIB_DIR_SUFFIX "${CMAKE_INSTALL_LIBDIR}")
 ENDIF (NOT DEFINED LIB_DIR_SUFFIX)
 
 # RPATH handling (see https://cmake.org/Wiki/CMake_RPATH_handling)
@@ -35,6 +29,6 @@ IF (APPLE)
 ELSE (APPLE)
 	SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
 ENDIF (APPLE)
-SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib${LIB_DIR_SUFFIX}/opencog")
+SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/opencog")
 SET(CMAKE_BUILD_WITH_INSTALL_NAME_DIR FALSE)
 SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
